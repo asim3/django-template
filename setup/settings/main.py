713831fcae504b3b0c76f43662a9019c616bc8e9
Asim <asimwebapps@gmail.com>
2021-07-10 21:16:18 +0300

    added SECURE_SSL_REDIRECT to settings

--- setup/settings/main.py
+++ setup/settings/main.py
@@ -58,3 +58,22 @@ LANGUAGES = [
 LOCALE_PATHS = (
     ( BASE_DIR / 'locale' ),
 )
+
+
+SECURE_SSL_REDIRECT = True
+
+
+SESSION_COOKIE_SECURE = True
+
+
+CSRF_COOKIE_SECURE = True
+
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+
+# for older browsers
+SECURE_BROWSER_XSS_FILTER = True
+
+
+SECURE_REFERRER_POLICY = 'same-origin'
