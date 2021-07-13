55e2c707e16a5d976f4b0b8491db699ca921011c
Asim <asimwebapps@gmail.com>
2021-07-13 17:21:41 +0300

    Redirect home

--- setup/apps/urls.py
+++ setup/apps/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = i18n_patterns(
     path('', include('products.urls')),
     path('admin/', admin.site.urls),
 )
+
+
+urlpatterns += [
+    path('', RedirectView.as_view(pattern_name='home')),
+]
