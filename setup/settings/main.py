36164484bec20d02a0e21500c16c7b1e00d7c567
Asim <asimwebapps@gmail.com>
2021-07-13 18:42:14 +0300

    update TEMPLATES settings

--- setup/settings/main.py
+++ setup/settings/main.py
@@ -19,21 +19,7 @@ INSTALLED_APPS += [
     'simple_history',
 ]
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
 
 DATABASES = {
     'default': {
