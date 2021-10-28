f61885d7e6d74c360b86dab8b1f188994bbf3a02
Asim <asimwebapps@gmail.com>
2021-10-28 21:15:05 +0300

    update static_resources

--- setup/static_resources/js/main.js
+++ setup/static_resources/js/main.js
@@ -6,16 +6,40 @@ String.prototype.clean_digits = function () {
 }
 
 
+// function is_there_QR_code_canvas() {
+//     QR_code_canvas = document.getElementById("QR_code_canvas")
+//     if (QR_code_canvas === null) {
+//         return false
+//     }
+//     else {
+//         return true
+//     }
+// }
+
+
+
+// function add_QR_code() {
+//     QR_code_canvas = document.getElementById("QR_code_canvas")
+//     url = window.location.href
+//     QR_code_canvas.innerHTML = ""
+//     new QRCode(QR_code_canvas, url);
+// }
+
+
+
 function _main(event) {
     const today = new Date()
     $("#all_rights_reserved").append(today.getFullYear())
 
+    // if (is_there_QR_code_canvas()) {
+    //     add_QR_code()
+    // }
 
-    document
-        .getElementById("id_select")
-        .addEventListener("change", select_changed)
+    // document
+    //     .getElementById("id_select")
+    //     .addEventListener("change", select_changed)
 
-    select_changed()
+    // select_changed()
 }
 
 
