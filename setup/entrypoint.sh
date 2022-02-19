ebb17aba761ac3900f68fbc9f3beeddef4f421ec
Asim <asimwebapps@gmail.com>
2022-02-20 00:53:57 +0300

    fix collectstatic --noinput in entrypoint.sh

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -10,7 +10,7 @@ echo "PostgreSQL started"
 
 python manage.py makemigrations \
   && python manage.py migrate \
-  && python manage.py collectstatic
+  && python manage.py collectstatic --noinput
 
 python manage.py runserver 0.0.0.0:8000
 
