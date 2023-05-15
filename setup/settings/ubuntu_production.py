048844e6456c799191ef46e69a38e49d77a9cd67
Asim <asimwebapps@gmail.com>
2023-05-16 00:07:29 +0300

    added ubuntu_production settings

---
+++ setup/settings/ubuntu_production.py
@@ -0,0 +1,31 @@
+import dj_database_url
+
+from .main import *
+
+
+DEBUG = False
+
+MAX_CONN_AGE = 600
+
+DATABASES = {
+    "default": dj_database_url.config(conn_max_age=MAX_CONN_AGE)
+}
+
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_SECONDS = 3600
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# for older browsers
+SECURE_BROWSER_XSS_FILTER = True
+
+SECURE_REFERRER_POLICY = 'same-origin'
