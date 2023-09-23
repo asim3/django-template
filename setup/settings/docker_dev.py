574df2a98c7fa21f9b71b19a197e64dbebb4a5b4
Asim <asimwebapps@gmail.com>
2023-09-23 11:35:31 +0300

    fix: DATABASE_URL environment @ Docker

--- setup/settings/docker_dev.py
+++ setup/settings/docker_dev.py
@@ -5,15 +5,16 @@ from .main import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
-MAX_CONN_AGE = 600
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
 # Ensure STATIC_ROOT exists.
 # os.makedirs(STATIC_ROOT, exist_ok=True)
 
 DATABASES = {
-    "default": dj_database_url.config(conn_max_age=MAX_CONN_AGE)
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 CAPTCHA_TEST_MODE = True
